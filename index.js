4a045cb1a1a8e6cf41a9b60c870f5512967d59a6
essential-transformation-0497 <samiraesquina@gmail.com>
2018-11-20 17:37:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ function yourAnimal() {
 
 function add2(n) {
   const two = 2
-  return 
+  return n + 2 
+}
 
   // Feel free to move things around!
-  const two = 2; 
-  return n + 2; 
-}
 
 var funkyFunction = function() {
   return function() {
@@ -29,4 +27,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = "FUNKY!";
+var theFunk = funkyFunction();
+theFunk = theFunk();
